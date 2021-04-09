678e6111a2ef2c434ecd86c6c07d0d79bad5239c
David McDonald <david.mcdonald@digital.cabinet-office.gov.uk>
2021-04-09 11:51:13 +0100

    wip

--- app/celery/broadcast_message_tasks.py
+++ app/celery/broadcast_message_tasks.py
@@ -149,6 +149,12 @@ def send_broadcast_event(broadcast_event_id):
 @notify_celery.task(bind=True, name="send-broadcast-provider-message", max_retries=None)
 @statsd(namespace="tasks")
 def send_broadcast_provider_message(self, broadcast_event_id, provider):
+    # if not current_app.config['CBC_PROXY_ENABLED']:
+    #     current_app.logger.info(
+    #         "CBC Proxy disabled, not sending broadcast_provider_message for "
+    #         f"broadcast_event_id {broadcast_event_id} with provider {provider}"
+    #     )
+    #     return
     broadcast_event = dao_get_broadcast_event_by_id(broadcast_event_id)
 
     check_provider_message_should_send(broadcast_event, provider)
