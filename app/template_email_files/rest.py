b7b7b2db4d82863e4e5c83acfb352322cffe8e3d
Richard Parke <richard.parke@digital.cabinet-office.gov.uk>
2025-11-11 14:51:46 +0000

    ruff format

--- app/template_email_files/rest.py
+++ app/template_email_files/rest.py
@@ -24,6 +24,7 @@ template_email_files_blueprint = Blueprint(
     "template_email_files", __name__, url_prefix="/service/<uuid:service_id>/<uuid:template_id>/template_email_files"
 )
 
+
 @template_email_files_blueprint.route("", methods=["POST"])
 def create_template_email_files(service_id, template_id):
     fetched_service = dao_fetch_service_by_id(service_id=service_id)
